3b7888cc0a3729ec7510c8e98082ac251aa21aaf
mateusbertolo <mateuscampos72@gmail.com>
2024-04-12 18:41:27 -0300

    feat photo

--- src/pet/interfaces/pet.repository.interface.ts
+++ src/pet/interfaces/pet.repository.interface.ts
@@ -1,6 +1,7 @@
 import { Pet } from '../schemas/pet.schema';
 
 export default interface IPetRepository {
+  [x: string]: any;
   create(data: Partial<Pet>): Promise<Pet>;
   getById(id: string): Promise<Pet>;
   deleteById(id: string): Promise<void>;
